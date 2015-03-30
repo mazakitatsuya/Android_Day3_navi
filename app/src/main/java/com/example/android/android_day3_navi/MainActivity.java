7190e107432ac1acb8d0fd6e3c1d08409561ddfb
mazaki.tatsuya <mazaki.tatsuya@dena.jp>
2015-03-30 12:25:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android/android_day3_navi/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.android.android_day3_navi;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //Navigationdrawerのメンバー変数
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Navigationdrawer設定
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setHomeButtonEnabled(true);
+        }
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, android.R.string.ok, android.R.string.cancel) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+    }
+
+
+    //Navigationdrawer設定
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        if (mDrawerToggle != null) {
+            mDrawerToggle.syncState();
+        }
+        super.onPostCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Navigationdrawer設定
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mDrawerToggle != null) {
+            mDrawerToggle.syncState();
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Navigationdrawer設定
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (mDrawerToggle != null) {
+            mDrawerToggle.syncState();
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
+}
